7f12032ac0f2f5fb4eb09a6cd46b3710c8169b6a
RRRishav <rishav6787@gmail.com>
2025-07-12 02:50:52 +0530

    dynamic routing

---
+++ src/app/studentlist/page.js
@@ -0,0 +1,17 @@
+import Link from "next/link";
+
+export default function StudentList() {
+    return <div>StudentList
+
+<ul>
+    <li><Link href="/studentlist/rishav">Rishav</Link></li>
+    <li><Link href="/studentlist/jane">Jane</Link></li>
+    <li><Link href="/studentlist/jim">Jim</Link></li>
+    <li><Link href="/studentlist/jill">Jill</Link></li>
+    <li><Link href="/studentlist/jack">Jack</Link></li>
+    <li><Link href="/studentlist/jill">Jill</Link></li>
+</ul>
+
+
+    </div>;
+}
